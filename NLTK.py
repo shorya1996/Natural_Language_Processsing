5fabc426921a75e7a3098b704966745126cef721
shorya sharma <ssharma1996@live.com>
2018-09-27 16:51:40 +0530

    learn the techniques like Bag of Words Model and work with NLTK

---
+++ NLTK.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 29 12:21:33 2018
+
+@author: ME DELL'
+"""
+
+"""
+NLP - NATURAL LANGUAGE PROCESSING
+
+NAIVE BAYES
+Text classification
+- SPAM classification 
+- Sentiment Analysis 
+- Topic Modelling"""
+
+'''text summarizers'''
+
+'''Text 
+Step 1. Tokenization - Word Tokenizer / Sentence Tokenizer 
+text = "hello this is a free seminar in delhi ncr" 
+token = ["hello" , "this", "is","a","free"....]
+
+Step 2. Remove stopwords (is,am,are,the,a,that,this)
+
+tokens = ["hello", "free", "seminar" , "delhi", "ncr"]
+
+step 3. Stemming / Lemmatizer
+porter stemmer/ snowball stemmer / lancaster stemmer
+[ playing = play 
+  wanted - want 
+]
+
+
+Step 4 -  converting text to numbers
+TF - IDF
+TF - Term Frequency  (count of particular word in sentence)
+IDF - Inverse Document Frequency (Impact of a word in document)
+
+
+Bag Of Words 
+
+search for best similariy measures on google
+
+'''
